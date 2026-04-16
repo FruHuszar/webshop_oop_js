3323210872f9323bdba900e93bbb608ab54a02b5
harnosvandaalina <harnosvandaalina@gmail.com>
2026-04-16 10:28:36 +0200

    index.js +index.html modositasok

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import TermekPublic from "./TermekPublic.js";
+import { termekLista } from "./termekLista.js";
+import TermekAdmin from "./TermekAdmin.js";
+
+
+const szuloELem = document. querySelector(".tarolo");
+
+new Termekek(termekLista, szuloELem)
