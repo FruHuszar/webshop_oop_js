9e02e66dd2b1282f5dc639789cb416f01df1286a
FruHuszar <fruzsinaanna.huszar@gmail.com>
2026-04-16 09:51:21 +0200

    init

---
+++ js/termekLista.js
@@ -0,0 +1,16 @@
+export const termekLista = [
+  {
+    nev: "Tulipán",
+    leiras: "lorem ipsum",
+    kep: "kepek/kep1.jpg",
+    ar: 3000,
+    ertekeles: "",
+  },
+  {
+    nev: "Ibolya",
+    leiras: "lorem ipsum",
+    kep: "kepek/kep2.webp",
+    ar: 5000,
+    ertekeles: "",
+  },
+];
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Webshop oop javascript
+
+## Feladatfelosztás
+
+UML, stilusok, index.html, TermekPublic.js: Vanda
+Readme, stilusok, admin.html, TermekAdmin.js: Fru
+
+```javascript
+//OOP szerkezet: termekLista, TermekAdmin, TermekPublic, Kosar
+```
