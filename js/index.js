38439ee6dd149ccc8fa1ddfc2507c14629f59575
Fru <fruzsinaanna.huszar@gmail.com>
2026-04-21 10:42:35 +0200

    Update index.js

--- js/index.js
+++ js/index.js
@@ -2,14 +2,12 @@ import { termekLista } from "./termekLista.js";
 import Termekek from "./Termekek.js";
 
 const szuloELem = document.querySelector(".tarolo");
+const isAdminOldal = window.location.pathname.includes("admin.html");
 
-new Termekek(termekLista, szuloELem, true);
+new Termekek(termekLista, szuloELem, isAdminOldal);
 
 window.addEventListener("torles", (event) => {
     const index = event.detail;
-    
     termekLista.splice(index, 1);
-    
-    szuloELem.innerHTML = "";
-    new Termekek(termekLista, szuloELem, true);
+    new Termekek(termekLista, szuloELem, isAdminOldal);
 });
